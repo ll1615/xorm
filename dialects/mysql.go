9f13122f28ac183d2ad242ff326dcf9623d4e78b
ll1615 <43992581@qq.com>
2020-03-21 15:13:59 +0800

    dialect mysql get columns by ddl order

--- dialects/mysql.go
+++ dialects/mysql.go
@@ -304,7 +304,8 @@ func (db *mysql) AddColumnSQL(tableName string, col *schemas.Column) string {
 func (db *mysql) GetColumns(ctx context.Context, tableName string) ([]string, map[string]*schemas.Column, error) {
 	args := []interface{}{db.uri.DBName, tableName}
 	s := "SELECT `COLUMN_NAME`, `IS_NULLABLE`, `COLUMN_DEFAULT`, `COLUMN_TYPE`," +
-		" `COLUMN_KEY`, `EXTRA`,`COLUMN_COMMENT` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA` = ? AND `TABLE_NAME` = ?"
+		" `COLUMN_KEY`, `EXTRA`,`COLUMN_COMMENT` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_SCHEMA` = ? AND `TABLE_NAME` = ?" +
+		" ORDER BY ORDINAL_POSITION ASC"
 
 	rows, err := db.DB().QueryContext(ctx, s, args...)
 	if err != nil {
